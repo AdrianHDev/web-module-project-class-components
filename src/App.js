4e08bd202c0ad066a44474442fb0d49105d97127
AdrianHDev <adrianhensondev@gmail.com>
2021-05-31 16:50:12 -0400

    Add initial structure for project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+import TodoList from "./components/TodoList"
+import TodoForm from "./components/TodoForm"
+
+const defaultTask = {
+  task: "",
+  id: 0,
+  completed: false,
+};
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -6,13 +14,27 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = {};
+    this.state = { tasks: [defaultTask] };
   }
+
+  clearCompleteTasks = ev => {
+    ev.preventDefault();
+    console.log(ev);
+    this.setState(state => {
+      return {tasks: state.tasks.filter(task => {
+        return task.completed === true;
+      })}
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList tasks={this.state.tasks}/>
+        <TodoForm clearCompleteTasks={this.clearCompleteTasks}/>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,13 @@
+import React from "react";
+import Todo from "./Todo"
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+class TodoList extends React.Component {
+  render() {
+    return <>{this.props.tasks.map((task) => {
+        return (<Todo task={task} />)
+    })}</>;
+  }
+}
+
+export default TodoList;
